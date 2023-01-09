667f15424ec86424292cd99983175be722bc67ae
Pandu-5 <118616769+Pandu-5@users.noreply.github.com>
2023-01-09 21:25:27 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,91 @@
+const prevBtns = document.querySelectorAll(".btn-pre");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step")
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum++;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+prevBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+        updateProgressbar();
+    });
+});
+
+function updateFormSteps() {
+    formSteps.forEach((formSteps => {
+        formSteps.classList.contains("form-step-active") &&
+            formSteps.classList.remove("form-step-active");
+    }))
+
+    formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressbar() {
+    progressSteps.forEach((progressSteps, index) => {
+        if (index < formStepsNum + 1) {
+            progressSteps.classList.add("progress-step-active");
+        } else {
+            progressSteps.classList.remove("progress-step-active");
+
+        }
+    });
+
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+
+}
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
+
+
+
+function myprofFun() {
+    var x = document.getElementById("content");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+const navBar = document.querySelector(".nav"),
+    menuBtns = document.querySelectorAll(".menu-icon"),
+    overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        navBar.classList.toggle("open");
+    });
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("open");
+});
